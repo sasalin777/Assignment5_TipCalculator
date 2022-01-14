29b43be7c5a9c5c7609f6640e52f3a3bb52e0926
sasalin777 <candysasame@gmail.com>
2022-01-14 08:23:38 -0800

    upload

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Li Tzu Lin on 2022-01-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
